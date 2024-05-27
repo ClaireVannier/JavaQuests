bea7def719a141e068deef11f9aa3bfb136ca93e
Claire Vannier <clairevannierpro@gmail.com>
2024-05-27 15:23:37 +0200

    finish quests 06

---
+++ src/ClassRoom.java
@@ -0,0 +1,13 @@
+public class ClassRoom {
+    public static void main(String[] args) {
+        Wilder emma = new Wilder("Emma", true);
+        Wilder jojo = new Wilder("Jojo", false);
+        Wilder claire = new Wilder("Claire", true);
+        Wilder john = new Wilder("John", false);
+
+        System.out.println(emma.whoAmI());
+        System.out.println(jojo.whoAmI());
+        System.out.println(claire.whoAmI());
+        System.out.println(john.whoAmI());
+    }
+}
